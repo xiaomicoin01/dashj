ef615051f1cfeb8f76d487d4ccbce3b7e129878f
HashEngineering <hashengineeringsolutions@gmail.com>
2019-03-15 18:28:16 -0700

    rename bitcoinj package to dashj (files)

--- migrate.sh
+++ migrate.sh
@@ -8,12 +8,15 @@ shopt -s globstar
 D=`date +%Y%m%d`
 CURVERSION=`grep '<artifactId>dashj-parent</artifactId>' -A1 pom.xml  | tail -1 | sed 's/.*<version>\(.*\)<\/version>.*/\1/'`
 
-sed -i "s/<version>$CURVERSION<\/version>/<version>$CURVERSION-$D<\/version>/" {,**/}pom.xml
-sed -i 's/org\.bitcoinj\./org\.dashj\./g' **/*.java **/*.proto
-sed -i 's/org\.bitcoin\([.;]\)/org\.dash\1/g' **/*.java
 rename -v 's/org\/bitcoinj/org\/dashj/' **/org/bitcoinj
 rename -v 's/org\/bitcoin/org\/dash/' **/org/bitcoin
 rename 's/org\.bitcoin\./org\.darkcoin\./' **
+git add .
+git commit -m "rename bitcoinj package to dashj (files)"
+
+sed -i "s/<version>$CURVERSION<\/version>/<version>$CURVERSION-$D<\/version>/" {,**/}pom.xml
+sed -i 's/org\.bitcoinj\./org\.dashj\./g' **/*.java **/*.proto
+sed -i 's/org\.bitcoin\([.;]\)/org\.dash\1/g' **/*.java
 
 git add .
-git commit -m "rename bitcoinj package to dashj"
+git commit -m "rename bitcoinj package to dashj (imports)"
